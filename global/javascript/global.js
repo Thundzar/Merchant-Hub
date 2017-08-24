3e42f1bf3b8033654d56ad13334fe71067a27315
Thundzar <Thundzar@users.noreply.github.com>
2017-08-23 17:10:54 -0700

    Update global.js

--- global/javascript/global.js
+++ global/javascript/global.js
@@ -1,1 +1,11 @@
+// Navbar Slider Animation Classes //
 
+$("#navbar-button-open").click(function() {
+  $(".site-navbar").removeClass("navbar-slidein");
+  $(".site-navbar").addClass("navbar-slideout");
+});
+
+$("#navbar-button-close").click(function(){
+  $(".site-navbar").removeClass("navbar-slideout");
+  $(".site-navbar").addClass("navbar-slidein");
+});
