620ff0870c0cd877c9d9b167e51cd78ebaf85cda
Thundzar <Thundzar@users.noreply.github.com>
2017-08-24 15:08:21 -0700

    + console logs for testing

--- global/javascript/global.js
+++ global/javascript/global.js
@@ -3,9 +3,15 @@
 $("#navbar-button-open").click(function() {
   $(".site-navbar").removeClass("navbar-slidein");
   $(".site-navbar").addClass("navbar-slideout");
+  
+  console.log("Open");
 });
 
 $("#navbar-button-close").click(function(){
   $(".site-navbar").removeClass("navbar-slideout");
   $(".site-navbar").addClass("navbar-slidein");
+  
+  console.log("Close");
 });
+
+console.log("Hello");
